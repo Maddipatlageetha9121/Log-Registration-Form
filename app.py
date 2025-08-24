d47c5a908fa1457c24ed7bc3b77206117cd2b4ba
Maddipatlageetha9121 <maddipatlageetha60@gmail.com>
2025-08-24 18:05:35 +0530

    updated code

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def register():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
 if __name__=='__main__':
-    app.run(debug='true',port=5000)
-    
+    app.run(debug='true',port=5000)    
